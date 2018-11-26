84998b406211768729e0e2cc0b796e78c0cf610f
Tom Zandt-Valentine <tzandt@googlemail.com>
2018-11-26 21:32:39 +0000

    Section 2: 16. Creating the projectiles explosion effect

--- Udemy_TZV_2DActionGame/Assets/projectile.cs
+++ Udemy_TZV_2DActionGame/Assets/projectile.cs
@@ -6,14 +6,22 @@ public class projectile : MonoBehaviour
 {
     public float speed = 15f;
     public float lifeTime = 2f;
+    public GameObject particleEffect;
 
     private void Start()
     {
-        Destroy(gameObject, lifeTime);
+        Invoke("DestroyProjectile", lifeTime);
     }
 
     private void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void DestroyProjectile()
+    {
+        Instantiate(particleEffect, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
     }
 }
